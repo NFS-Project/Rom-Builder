350edc09687650a86900dd921c94f477cb99afdb
zacky <103025109+zacky46@users.noreply.github.com>
2022-10-15 16:46:09 +0700

    build: bananadroid A12L

--- whyred/build_rom.sh
+++ whyred/build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-Project/local_manifest --depth 1 -b banana-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -12,4 +12,4 @@ export ALLOW_MISSING_DEPENDENCIES=true
 lunch banana_ginkgo-user
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>($(grep unch $HOME/script/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1))</b> Building Started%0A<b>ROM:</b> $rom_name%0A$(echo "${var_cache_report_config}")"
 m banana
-
+# end
