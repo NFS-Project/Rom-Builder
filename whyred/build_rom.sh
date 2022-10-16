9605bf1526c5fc2f6d7050bf280d5aa6dc684563
zacky <103025109+zacky46@users.noreply.github.com>
2022-10-16 14:22:13 +0700

    Update build_rom.sh

--- whyred/build_rom.sh
+++ whyred/build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10
 git clone https://github.com/yanzszs/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
